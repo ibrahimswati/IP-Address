3d8e83706d9c3101e9c3aaaf6bbc4884258139fa
Ibrahim Swati <63630826+ibrahimswati@users.noreply.github.com>
2022-02-02 11:21:25 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 import urllib.request
 from flask import json
+import re
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
     #access_key = ""
-    api = "http://api.ipstack.com/check?access_key=37a43eb5e02577a7ede5e49de1e5b330&output=json&fields=ip,city,zip,latitude,longitude"
-    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR')
-    response = urllib.request.urlopen(api).read().decode('UTF-8')
-    data = json.loads(response)
+    ip = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    ip_space = ip.replace(" ", "")
+    ip_split = ip_space.split(",", 1)
+    ip_api = ip_split[0]
+    check_api = "http://api.ipstack.com/check?access_key=7bb51c62bdd1247a480ed6f5c972635f&output=json&fields=ip,city,zip,latitude,longitude"
+    api = "http://api.ipstack.com/" + ip_api + "?access_key=7bb51c62bdd1247a480ed6f5c972635f&output=json&fields=ip,city,zip,latitude,longitude"
+
+    check_response = urllib.request.urlopen(check_api).read().decode('UTF-8')
+    check_data = json.loads(check_response)
+    stackip = (check_data['ip'])
+    stackcity = (check_data['city'])
+    stackzip = (check_data['zip'])
+
+    ip_response = urllib.request.urlopen(api).read().decode('UTF-8')
+    ip_data = json.loads(ip_response)
+    stack_ip = (ip_data['ip'])
+    stack_city = (ip_data['city'])
+    stack_zip = (ip_data['zip'])
+
 
     return (
-    (data['ip'])
-    +(data['city'])
-    +(data['zip'])
+    render_template('index.html', **locals())
     )
-
 
 
 
